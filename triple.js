7ed70fd2d66b6a064967519296928ea6d82ca0a0
AlAminMiah4274 <alaminmiah4274@gmail.com>
2022-12-21 17:45:43 +0600

    discussed about 2 & 3 equal

---
+++ triple.js
@@ -0,0 +1,12 @@
+const first = 2;
+const second = 2;
+if (first == second) {
+    console.log('Value is equal');
+}
+else {
+    console.log('value is not equal');
+}
+
+// type coercion
+// type conversion
+// type casting
