566fbc4f8d400a684e72daf1756e3996c52ecb9d
AlAminMiah4274 <alaminmiah4274@gmail.com>
2022-12-22 20:53:19 +0600

    discussed about hoisting

---
+++ hoisting.js
@@ -0,0 +1,17 @@
+// print5();
+print10();
+
+for (let i = 0; i < 5; i++) {
+    // console.log(i);
+}
+// console.log('Outside:', i)
+
+function print5() {
+    console.log('inside:', 5);
+}
+
+var print10 = function () {
+    console.log('inside:', 10);
+}
+
+// print10();
