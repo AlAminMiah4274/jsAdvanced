80d2d1100d428bafa8af06d62ca4d595aa312d1d
AlAminMiah4274 <alaminmiah4274@gmail.com>
2022-12-23 15:42:40 +0600

    nothing changed

--- passBy.js
+++ passBy.js
@@ -8,9 +8,9 @@ function multiply(a, b) {
     const result = a * b;
     return result;
 }
-console.log(num1);
+// console.log(num1);
 multiply(num1, num2);
-console.log(num1);
+// console.log(num1);
 
 // non-primitive types are passed by reference.
 let student1 = { name: 'Jalil', partner: 'Borsha' };
@@ -20,6 +20,6 @@ function makeMovie(couple1, couple2) {
     couple1.name = 'Ananta';
     couple2.partner = 'Mim';
 }
-console.log(student1, student2);
+// console.log(student1, student2);
 makeMovie(student1, student2);
-console.log(student1, student2);
+// console.log(student1, student2);
